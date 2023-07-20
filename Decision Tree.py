af0cea64e4a44a6b2a245005130040e889189676
Si Yang Chen <sysy0115@1utar.my>
2023-07-20 09:29:16 +0800

    Adding data transformation

--- Decision Tree.py
+++ Decision Tree.py
@@ -3,38 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict, GridSearchCV
 from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_curve, roc_curve, roc_auc_score, confusion_matrix, accuracy_score
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils.multiclass import unique_labels
 from imblearn.over_sampling import SMOTE
-
+import pickle
 
 # Data Exploration
 dataset = pd.read_csv("WISDM_at_v2.0_transformed.csv")
-"""
-pd.options.display.max_columns = None
-print("Head of Dataset\n", dataset.head(10))
-print("Info of Dataset\n", dataset.info())
-print("Description of Dataset\n", dataset.describe())
-print(pd.crosstab(index=dataset["Activity"], columns="count"))
-plt.figure(figsize=(10,5))
-ax = sns.countplot(x="Activity", data=dataset)
-plt.xticks(x=dataset['Activity'],  rotation='vertical')
-plt.show(
-)
-"""
 
-# Data Preprocessing
 # Splitting Dataset into x and y
-x = dataset.drop("Activity", axis=1)
+x = dataset.drop(["Activity", "user", "id"], axis=1)
 y = dataset["Activity"]
 
-"""
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y)
-"""
-
+# Data Preprocessing
 scaler = StandardScaler()
 x = scaler.fit_transform(x)
 
@@ -48,90 +30,7 @@ x_train, y_train = smote.fit_resample(x_train, y_train)
 # Decision Tree Model
 tree_clf = DecisionTreeClassifier(random_state=42)
 tree_clf.fit(x_train, y_train)
-y_tree = tree_clf.predict(x_train)
 
-# Accuracy Before Performing Cross Validation
-train_acc = accuracy_score(y_tree, y_train)
-print("\nTraining Accuracy before CV: {:.4f}".format(train_acc))
+pickle.dump(tree_clf, open('model.pkl','wb'))
 
-# Confusion Matrix Before Performing Cross Validation
-cm = confusion_matrix(y_train, y_tree)
-print("\nConfusion Matrix before CV:\n", cm)
-
-# Performing Cross Validation
-k_scores = cross_val_score(tree_clf, x_train, y_train, cv=5, scoring='accuracy')
-print("\nTraining Accuracy after CV:\n", k_scores)
-
-# Confusion Matrix
-y_tree_cv = cross_val_predict(tree_clf, x_train, y_train, cv=5)
-cm = confusion_matrix(y_train, y_tree_cv)
-print("\nConfusion Matrix after CV:\n", cm)
-classes = unique_labels(y_train, y_tree_cv)
-
-# Create figure and axis
-fig, ax = plt.subplots()
-im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-ax.figure.colorbar(im, ax=ax)
-
-# Add title, labels, and tick marks
-ax.set(title='Decision Tree',
-       xlabel='Predicted Value',
-       ylabel='Actual Value')
-tick_marks = np.arange(len(classes))
-ax.set(xticks=tick_marks, xticklabels=classes,
-       yticks=tick_marks, yticklabels=classes)
-
-# Rotate the tick labels for better readability
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
-
-# Loop over data dimensions and create text annotations
-thresh = cm.max() / 2.
-for i in range(cm.shape[0]):
-    for j in range(cm.shape[1]):
-        ax.text(j, i, format(cm[i, j], 'd'),
-                ha="center", va="center",
-                color="white" if cm[i, j] > thresh else "black")
-
-# Show the plot
-plt.tight_layout()
-plt.show()
-
-# Per Class Accuracy
-matrix = confusion_matrix(y_train, y_tree_cv)
-print(matrix.diagonal()/matrix.sum(axis=1))
-
-# Measure the Accuracy, Precision, Recall and f1 score
-print('\nAccuracy  = {:.4f}'.format(accuracy_score(y_train, y_tree_cv)))
-print('Precision = {:.4f}'.format(precision_score(y_train, y_tree_cv, average='macro')))
-print('Recall    = {:.4f}'.format(recall_score(y_train, y_tree_cv, average='macro')))
-print('f1 score  = {:.4f}'.format(f1_score(y_train, y_tree_cv, average='macro')))
-
-
-# # Fine-Tune the Model Using Grid Search
-# param_grid = {
-#     'max_depth': [2, 3, 5, 10, 20],
-#     'min_samples_leaf': [5, 10, 20, 50, 100],
-#     'criterion': ["gini", "entropy"]
-# }
-# grid_search = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=5, scoring='accuracy')
-# print ('Performing grid search...')
-# grid_search.fit(x_train, y_train)
-# print('done')
-
-# # View the accuracy score
-# print('Best score for training data:', grid_search.best_score_, "\n")
-
-# # View the best parameters for the model found using grid search
-# print('Best Parameter:', grid_search.best_params_)
-# #'criterion': 'gini', 'max_depth': 20, 'min_samples_leaf': 5
-
-# final_model = grid_search.best_estimator_
-# print(final_model)
-
-# # Predicting the test set
-# y_test = final_model.predict(x_test)
-# print('Test accuracy = {:.4f}'.format(accuracy_score(y_test, y_test)))
-# print('Test precision = {:.4f}'.format(precision_score(y_test, y_test, average='macro')))
-# print('Test recall = {:.4f}'.format(recall_score (y_test, y_test, average='macro')))
-# print('Test f1 score = {:.4f}'.format(f1_score(y_test, y_test, average='macro')))
+model = pickle.load(open('model.pkl','rb'))
--- app.py
+++ app.py
@@ -1,34 +1,111 @@
-
-import numpy as np
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS  # Import the CORS module
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 import pickle
+from scipy.signal import find_peaks
 
 app = Flask(__name__)
+CORS(app)  # Apply CORS to your Flask app
+
+# Load the machine learning model
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/result', methods=['POST'])
+def receive_accelerometer_data():
+    data = request.get_json()
 
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    x_data = preprocess_data(data)
 
-    output = round(prediction[0], 2)
+    # Make predictions using the loaded machine learning model
+    prediction = model.predict(x_data)
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    # Convert prediction to a Python list
+    prediction = prediction.tolist()
 
-@app.route('/results',methods=['POST'])
-def results():
+    # Render the index.html
+    return prediction[0]
 
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+# Data Preprocessing function
+def preprocess_data(data):
+    # Assuming data is a dictionary with keys 'x', 'y', and 'z'
+    # Convert data into a pandas DataFrame
+    df = pd.DataFrame(data, index=[0])
 
-    output = prediction[0]
-    return jsonify(output)
+    # Average acceleration (for each axis)
+    df['XAVG'] = df['x'].mean()
+    df['YAVG'] = df['y'].mean()
+    df['ZAVG'] = df['z'].mean()
 
-if __name__ == "__main__":
+    # Standard Deviation for each axis
+    df['XSTANDDEV'] = df['x'].std()
+    df['YSTANDDEV'] = df['y'].std()
+    df['ZSTANDDEV'] = df['z'].std()
+
+    # Calculate Average Absolute Difference for each axis
+    df['XABSOLDEV'] = np.mean(np.absolute(df['x'] - df['x'].mean()))
+    df['YABSOLDEV'] = np.mean(np.absolute(df['y'] - df['y'].mean()))
+    df['ZABSOLDEV'] = np.mean(np.absolute(df['z'] - df['z'].mean()))
+
+    # Calculate Average Resultant Acceleration
+    df['RESULTANT'] = np.mean(np.sqrt(df['x']**2 + df['y']**2 + df['z']**2))
+
+    # Calculate Time Between Peaks for each axis
+    fs = 20
+    time_values = np.arange(len(df)) / fs
+
+    # Find peaks for each axis
+    # height = 0 -> Find local maxima
+    x_peaks, _ = find_peaks(df['x'], height=0)
+    y_peaks, _ = find_peaks(df['y'], height=0)
+    z_peaks, _ = find_peaks(df['z'], height=0)
+
+    # Calculate Time Between Peaks for each axis
+    df['XPEAK'] = np.mean(np.diff(time_values[x_peaks]))
+    df['YPEAK'] = np.mean(np.diff(time_values[y_peaks]))
+    df['ZPEAK'] = np.mean(np.diff(time_values[z_peaks]))
+
+    # Calculate Binned Distribution for each axis
+    num_bins = 10
+    # Calculate Binned Distribution for each axis
+    num_bins = 10
+    x_bins = calculate_binned_distribution(df['x'], num_bins)
+    y_bins = calculate_binned_distribution(df['y'], num_bins)
+    z_bins = calculate_binned_distribution(df['z'], num_bins)
+
+    # Create separate columns for each binned distribution feature for each axis
+    for i in range(num_bins):
+        df[f'X{i}'] = x_bins[i]
+        df[f'Y{i}'] = y_bins[i]
+        df[f'Z{i}'] = z_bins[i]
+
+    # Drop raw data
+    df = df.drop(['x', 'y', 'z'], axis=1) 
+
+    # Data Preprocessing 
+    scaler = StandardScaler()
+    x = df.values
+    x = scaler.fit_transform(x)
+
+    print(df.info())
+    print(df[["XPEAK", "YPEAK", "ZPEAK", "XSTANDDEV", "YSTANDDEV", "ZSTANDDEV"]])
+
+    # Convert the NumPy array to a Python list
+    return x.tolist()  
+
+# Calculate Binned Distribution for each axis
+def calculate_binned_distribution(data, num_bins=10):
+    min_val = data.min()
+    max_val = data.max()
+    bin_range = (max_val - min_val) / num_bins
+
+    bins = [min_val + i * bin_range for i in range(num_bins + 1)]
+    return np.histogram(data, bins=bins)[0] / len(data)
+
+if __name__ == '__main__':
     app.run(debug=True)
